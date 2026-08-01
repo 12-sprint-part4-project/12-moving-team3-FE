71bee578b2eae2b21c84dd29ccb40593714f1db8
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-08-01 09:44:52 +0900

    fix: 오타 수정

--- src/app/(main)/(customer)/estimates/request/_components/steps/AddressStep.tsx
+++ src/app/(main)/(customer)/estimates/request/_components/steps/AddressStep.tsx
@@ -6,7 +6,7 @@ const AddressStep = () => {
   return (
     <section
       aria-label="출발지 도착지 입력"
-      className="flexmx-auto w-full max-w-[375px] flex-col gap-3 px-6 md:max-w-[1448px]"
+      className="mx-auto flex w-full max-w-[375px] flex-col gap-3 px-6 md:max-w-[1448px]"
     >
       <p className="text-lg-medium text-black-400">
         출발지와 도착지를 입력해 주세요. (스텝3 스텁)
